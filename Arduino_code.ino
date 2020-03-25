65c2cc3c6407f75c7e223ab8c42909df967632f9
0LTSU0 <55877194+0LTSU0@users.noreply.github.com>
2020-03-25 12:45:23 +0200

    Update Arduino_code.ino

--- Arduino_code.ino
+++ Arduino_code.ino
@@ -1,1 +1,117 @@
-//hello
+//this code only drives motors, no display things
+
+const int pingPin = 2;
+
+int enA = 9;
+int in1 = 8;
+int in2 = 7;
+
+int enB = 3;
+int in3 = 5;
+int in4 = 4;
+
+void setup() {
+  
+  	Serial.begin(9600);
+  	pinMode(enA, OUTPUT);
+	pinMode(enB, OUTPUT);
+	pinMode(in1, OUTPUT);
+	pinMode(in2, OUTPUT);
+	pinMode(in3, OUTPUT);
+	pinMode(in4, OUTPUT);
+	
+	// Turn off motors - Initial state
+	digitalWrite(in1, LOW);
+	digitalWrite(in2, LOW);
+	digitalWrite(in3, LOW);
+	digitalWrite(in4, LOW);
+}
+
+void loop() {
+
+  
+  long duration, inches, cm;
+  
+  pinMode(pingPin, OUTPUT);
+  digitalWrite(pingPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(pingPin, HIGH);
+  delayMicroseconds(5);
+  digitalWrite(pingPin, LOW);
+  
+  pinMode(pingPin, INPUT);
+  duration = pulseIn(pingPin, HIGH);
+  
+  cm = microsecondsToCentimeters(duration);
+  
+  Serial.print(cm);
+  Serial.print("cm");
+  Serial.println();
+
+  delay(100);
+  
+  directionControl();
+  delay(1000);
+  speedControl();
+  delay(1000);
+  
+}
+
+long microsecondsToCentimeters(long microseconds) { return microseconds / 29 / 2;
+}
+
+void directionControl() {
+	// Set motors to maximum speed
+	// For PWM maximum possible values are 0 to 255
+	analogWrite(enA, 255);
+	analogWrite(enB, 255);
+
+	// Turn on motor A & B
+	digitalWrite(in1, HIGH);
+	digitalWrite(in2, LOW);
+	digitalWrite(in3, HIGH);
+	digitalWrite(in4, LOW);
+	delay(2000);
+	
+	// Now change motor directions
+	digitalWrite(in1, LOW);
+	digitalWrite(in2, HIGH);
+	digitalWrite(in3, LOW);
+	digitalWrite(in4, HIGH);
+	delay(2000);
+	
+	// Turn off motors
+	digitalWrite(in1, LOW);
+	digitalWrite(in2, LOW);
+	digitalWrite(in3, LOW);
+	digitalWrite(in4, LOW);
+}
+
+// This function lets you control speed of the motors
+void speedControl() {
+	// Turn on motors
+	digitalWrite(in1, LOW);
+	digitalWrite(in2, HIGH);
+	digitalWrite(in3, LOW);
+	digitalWrite(in4, HIGH);
+	
+	// Accelerate from zero to maximum speed
+	for (int i = 0; i < 256; i++) {
+		analogWrite(enA, i);
+		analogWrite(enB, i);
+		delay(20);
+	}
+	
+	// Decelerate from maximum speed to zero
+	for (int i = 255; i >= 0; --i) {
+		analogWrite(enA, i);
+		analogWrite(enB, i);
+		delay(20);
+	}
+	
+	// Now turn off motors
+	digitalWrite(in1, LOW);
+	digitalWrite(in2, LOW);
+	digitalWrite(in3, LOW);
+	digitalWrite(in4, LOW);
+}
