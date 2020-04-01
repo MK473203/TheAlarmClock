b0bf56057fcc2e98c85cd93727016071ed6cae1a
MK473203 <mk473203@outlook.com>
2020-04-01 12:05:12 +0300

    Update Arduino_code_movement.ino

--- Arduino_code_movement.ino
+++ Arduino_code_movement.ino
@@ -1,12 +1,12 @@
 //CODE FOR ARDUINO THAT IS RESPOSIBLE FOR MOVEMENT
-
 #define trigPin 12
 #define echoPin 11
 
-char serial[10];
+char serial[1];
 
 long duration;
 int distance;
+int halytys = 0;
 
 int enA = 9;
 int in1 = 8;
@@ -18,22 +18,39 @@ int in4 = 4;
 
 void setup() {
   	Serial.begin(9600);
+	pinMode(enA, OUTPUT);
+	pinMode(enB, OUTPUT);
+	pinMode(in1, OUTPUT);
+	pinMode(in2, OUTPUT);
+	pinMode(in3, OUTPUT);
+	pinMode(in4, OUTPUT);
+  	pinMode(10, OUTPUT);
+    pinMode(13, INPUT);
   
-  	if (Serial.readBytes(serial, 1) == 1) {
-		pinMode(enA, OUTPUT);
-		pinMode(enB, OUTPUT);
-		pinMode(in1, OUTPUT);
-		pinMode(in2, OUTPUT);
-		pinMode(in3, OUTPUT);
-		pinMode(in4, OUTPUT);
-  
-  		pinMode(trigPin, OUTPUT);
-  		pinMode(echoPin, INPUT);
-    }
+	pinMode(trigPin, OUTPUT);
+  	pinMode(echoPin, INPUT);
 }
 
 void loop() {
-
+  /*Serial.readBytes(serial, 1);
+  if (serial[0] == '1' && halytys == 0){
+    halytys == 1;
+  }
+  
+  if (serial[0] == '2' && halytys == 1){
+    halytys == 0;
+  }*/
+  
+  if(digitalRead(13) == HIGH && halytys == 0) {
+  	halytys = 1;
+    Serial.println("Alarm on");
+  }
+  if(digitalRead(13) == LOW && halytys == 1) {
+  	halytys = 0;
+    Serial.println("Alarm off");
+  }
+   
+  if (halytys == 1){
   	analogWrite(enA, 180);
 	analogWrite(enB, 180);
 
@@ -55,8 +72,10 @@ void loop() {
   	Serial.println(" cm");
   
   	delay(50);
+    
+    //tone(10, 330);
   
-  if (distance < 20) {
+  	if (distance < 50) {
     digitalWrite(in1, LOW);
 	digitalWrite(in2, LOW);
 	digitalWrite(in3, LOW);
@@ -69,5 +88,11 @@ void loop() {
 	digitalWrite(in3, LOW);
 	digitalWrite(in4, HIGH);
 	delay(500);
+    }
+  } else if(halytys == 0) {
+    digitalWrite(in1, LOW);
+	digitalWrite(in2, LOW);
+	digitalWrite(in3, LOW);
+	digitalWrite(in4, LOW);
   }
 }
