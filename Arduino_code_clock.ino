57045173d3bcc217992ba20c6baa9ca758e52544
MK473203 <mk473203@outlook.com>
2020-04-15 11:29:53 +0300

    Update Arduino_code_clock.ino

--- Arduino_code_clock.ino
+++ Arduino_code_clock.ino
@@ -2,7 +2,23 @@
 
 #include <LiquidCrystal.h>
 
-// initialize the library with the numbers of the interface pins and intialize all other necessary pins
+/*
+  The circuit:
+ * LCD RS pin to digital pin 12
+ * LCD Enable pin to digital pin 11
+ * LCD D4 pin to digital pin 5
+ * LCD D5 pin to digital pin 4
+ * LCD D6 pin to digital pin 3
+ * LCD D7 pin to digital pin 2
+ * LCD R/W pin to ground
+ * LCD VSS pin to ground
+ * LCD VCC pin to 5V
+ * 10K resistor:
+ * ends to +5V and ground
+ * wiper to LCD VO pin (pin 3)
+*/
+
+// initialize the library with the numbers of the interface pins
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
 const int plusButton = 10;
@@ -12,169 +28,163 @@ int plusButtonState = 0;
 int modeButtonState = 0;
 int minusButtonState = 0;
 
-int mode = 0;			// 0 for normal clock,
+int mode = 0;	// 0 for normal clock,
 				// 1 for changing hours, 
 				// 2 for minutes,
-				// 3 for setting alarm time,
-				// 4 for enabling/disabling alarm.
+				// 3 for enabling/disabling alarm,
+				// 4 for setting alarm time.
 
-const int timeScale = 100; // for testing purposes, this controls the speed of how fast time is moving
-
+const int timeScale = 180; // for testing purposes, this controls the speed of how fast time is moving
 
 //intialize time etc. when powered up
 int minutesSinceStart = 0;
 int hoursSinceStart = 0;
 
-int startMinutes = 55;
-int startHours = 23;
+int startMinutes = 0;
+int startHours = 0;
 
-int alarmMinutes = 0;
-int alarmHours = 0;
+int alarmMinutes = 00;
+int alarmHours = 8;
 
-bool useAlarm = true;
+bool useAlarm = false;
 
 bool isAlarming = false;
 
 char timeText[10];
 
 void setup() {
-	 Serial.begin(9600);
+  Serial.begin(9600);
   
-  	pinMode(plusButton, INPUT);
-  	pinMode(modeButton, INPUT);
-  	pinMode(minusButton, INPUT);
-  	pinMode(13, OUTPUT);
-  	lcd.begin(16, 2);
-  	lcd.print("Time:");
+  pinMode(plusButton, INPUT);
+  pinMode(modeButton, INPUT);
+  pinMode(minusButton, INPUT);
+  pinMode(13, OUTPUT);
+  lcd.begin(16, 2);
+  lcd.print("Time:");
 }
 
 void loop() {
   
-  	plusButtonState = digitalRead(plusButton);
-  	modeButtonState = digitalRead(modeButton);
-  	minusButtonState = digitalRead(minusButton);
+  plusButtonState = digitalRead(plusButton);
+  modeButtonState = digitalRead(modeButton);
+  minusButtonState = digitalRead(minusButton);
   
-	
-	//if plusButton is pressed, depending of the mode of the clock either nothing happens or hours/minutes/alarm time is advanced
-  	if(plusButtonState == HIGH) {
-    		switch (mode) {
-      		case 0:
-      			break;
-      		case 1:
-      			startHours = (startHours + 1) % 24;
-      			break;
-      		case 2:
-      			startMinutes = (startMinutes + 1) % 60;
-      			break;
-      		case 3:
-      			alarmMinutes = alarmMinutes + 1;
-      			if(alarmMinutes >= 60) {
-        		alarmHours = (alarmHours + 1) % 24;
-        		alarmMinutes = 0;
-      			}
-      			break;
-      		case 4:
-      			useAlarm = true;
-      			mode = 0;
-      			break;
-    		}
-  	}
-  	
-	//if modeButton (midlle one) is pressed, mode of the clock changes see meanings of different values 0-5 above
-	if(modeButtonState == HIGH) {
-    		mode = (mode + 1) % 5;
-  	}
-	
-	//if minusButton is pressed, depending of the mode of the clock either nothing happens or hours/minutes/alarm time is decreased
-  	if(minusButtonState == HIGH) {
-    		switch (mode) {
-      		case 0:
-      			break;
-      		case 1:
-      			startHours = startHours - 1;
-      			if(startHours < 0) startHours = 23;
-      			break;
-      		case 2:
-      			startMinutes = startMinutes - 1;
-      			if(startMinutes < 0) startMinutes = 59;
-      			break;
-      		case 3:
-      			alarmMinutes = alarmMinutes - 1;
-      			if(alarmMinutes < 0) {
-      			alarmHours = alarmHours - 1;
-      			if(alarmHours < 0) alarmHours = 23;
-        		alarmMinutes = 59;
-      			}
-      			break;
-      		case 4:
-      			useAlarm = false;
-      			mode = 0;
-      			break;
-    		}	
- 	}
+  //if plusButton is pressed, depending of the mode of the clock either nothing happens or hours/minutes/alarm time is advanced
+  if(plusButtonState == HIGH) {
+    switch (mode) {
+      case 0:
+      	break;
+      case 1:
+      	startHours = (startHours + 1) % 24;
+      	break;
+      case 2:
+      	startMinutes = (startMinutes + 1) % 60;
+      	break;
+      case 3:
+      	useAlarm = true;
+      	mode = 4;
+      	break;
+      case 4:
+      	alarmMinutes = alarmMinutes + 1;
+      	if(alarmMinutes >= 60) {
+        	alarmHours = (alarmHours + 1) % 24;
+        	alarmMinutes = 0;
+      	}
+      	break;
+    }
+  }
   
-	//Text to be printed on screen for different modes
-	switch (mode) {
-  		case 0:
-  			lcd.setCursor(0,0);
-      			lcd.print("Time:           ");
-      			break;
-    		case 1:
-      			lcd.setCursor(0,0);
-      			lcd.print("Set hours       ");
-      			break;
-    		case 2:
-      			lcd.setCursor(0,0);
-      			lcd.print("Set minutes     ");
-      			break;
-    		case 3:
-      			lcd.setCursor(0,0);
-      			lcd.print("Set alarm       ");
-      			break;
-    		case 4:
-      			lcd.setCursor(0,0);
-      			lcd.print("   Use alarm?   ");
-      			lcd.setCursor(0,1);
-      			lcd.print("No           Yes");
-      			break;
-    	}
+  //if modeButton (midlle one) is pressed, mode of the clock changes see meanings of different values 0-5 above
+  if(modeButtonState == HIGH) {
+    mode = min(mode + 1, 4) % 4;
+  }
   
-  	//count passed time since powering up
-  	minutesSinceStart = millis() * timeScale / 60000;
-  	hoursSinceStart = (startMinutes + minutesSinceStart) / 60;
+  //if minusButton is pressed, depending of the mode of the clock either nothing happens or hours/minutes/alarm time is decreased
+  if(minusButtonState == HIGH) {
+    switch (mode) {
+      case 0:
+      	break;
+      case 1:
+      	startHours = startHours - 1;
+      	if(startHours < 0) startHours = 23;
+      	break;
+      case 2:
+      	startMinutes = startMinutes - 1;
+      	if(startMinutes < 0) startMinutes = 59;
+      	break;
+      case 3:
+      	useAlarm = false;
+      	mode = 0;
+      	break;
+      case 4:
+      	alarmMinutes = alarmMinutes - 1;
+      	if(alarmMinutes < 0) {
+      		alarmHours = alarmHours - 1;
+      		if(alarmHours < 0) alarmHours = 23;
+        	alarmMinutes = 59;
+      	}
+      	break;
+    }
+  }
   
-	//if alarm triggers, send signal to other arduino
-  	if(useAlarm) {
+  //Text to be printed on screen for different modes
+  switch (mode) {
+  	case 0:
+  		lcd.setCursor(0,0);
+      	lcd.print("Time:           ");
+      	break;
+    case 1:
+      	lcd.setCursor(0,0);
+      	lcd.print("Set hours       ");
+      	break;
+    case 2:
+      	lcd.setCursor(0,0);
+      	lcd.print("Set minutes     ");
+      	break;
+    case 3:
+      	lcd.setCursor(0,0);
+      	lcd.print("   Use alarm?   ");
+      	lcd.setCursor(0,1);
+      	lcd.print("No           Yes");
+      	break;
+    case 4:
+      	lcd.setCursor(0,0);
+      	lcd.print("Set alarm       ");
+      	break;
+    }
   
-    		if((startHours+hoursSinceStart) % 24 == alarmHours && (startMinutes+minutesSinceStart) % 60 == alarmMinutes && isAlarming == false) {
-      		digitalWrite(13, HIGH);
-      		isAlarming = true;
-    	}
+  //count passed time since powering up
+  minutesSinceStart = millis() * timeScale / 60000;
+  hoursSinceStart = (startMinutes + minutesSinceStart) / 60;
   
-	//if alarm is disabled by pressing any button, make other arduino stop what it's doing
-    	if(plusButtonState == HIGH || modeButtonState == HIGH || minusButtonState == HIGH) {
-      		digitalWrite(13, LOW);
-      		isAlarming = false;
-    	}	
+  if(useAlarm) {
+  	//if alarm triggers, send signal to other arduino
+    if((startHours+hoursSinceStart) % 24 == alarmHours && (startMinutes+minutesSinceStart) % 60 == alarmMinutes && isAlarming == false) {
+      digitalWrite(13, HIGH);
+      isAlarming = true;
+    }
+  	//if alarm is disabled by pressing any button, make other arduino stop what it's doing
+    if(plusButtonState == HIGH || modeButtonState == HIGH || minusButtonState == HIGH) {
+      digitalWrite(13, LOW);
+      isAlarming = false;
+    }
     
-  	}
+  }
   
-	
-	//Values to be printed on screen in different modes
-  	lcd.setCursor(0, 1);
+  //Values to be printed on screen in different modes
+  lcd.setCursor(0, 1);
   
-  	if(mode <= 2) {
-  		sprintf(timeText, "%02d:%02d           ", 
-           	(startHours+hoursSinceStart) % 24,
-           	(startMinutes+minutesSinceStart) % 60);
-  		lcd.print(timeText);
-  		} 
-	else if (mode == 3) {
-  		sprintf(timeText, "%02d:%02d           ", 
-            	alarmHours,
-            	alarmMinutes);
-  		lcd.print(timeText);
-  	}
+  if(mode <= 2) {
+  	sprintf(timeText, "%02d:%02d           ", 
+           (startHours+hoursSinceStart) % 24,
+           (startMinutes+minutesSinceStart) % 60);
+  	lcd.print(timeText);
+  } else if (mode == 4) {
+  	sprintf(timeText, "%02d:%02d           ", 
+            alarmHours,
+            alarmMinutes);
+  	lcd.print(timeText);
+  }
   
-  	delay(50);
+  delay(50);
 }
